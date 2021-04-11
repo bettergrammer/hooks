600600c9eba8ffc170eb8c19a5b61b971ec9a16a
bettergrammer <wiesnerjjason@gmail.com>
2021-04-11 16:00:56 -0700

    Add Decrement button

---
+++ client/app.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    console.log('side effect')
+  }, [])
+
+
+  return (
+    <div className='main-app'>
+      <div>
+        {count}
+      </div>
+      <button type='button' onClick={() => setCount(count + 1)} >Increment</button>
+      <button type='button' onClick={() => setCount(count - 1)} >Decrement</button>
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
