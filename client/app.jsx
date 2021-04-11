e489cf9637f52d1ca91252ee837ee9a23a9e6ac8
bettergrammer <wiesnerjjason@gmail.com>
2021-04-11 16:45:18 -0700

    add more state

--- client/app.jsx
+++ client/app.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 function App() {
 
-  const [count, setCount] = useState(1);
+  const [{count, count2}, setCount] = useState({ count: 1, count2: 0});
 
   useEffect(() => {
     console.log('side effect');
@@ -13,9 +13,15 @@ function App() {
   return (
     <div className='main-app'>
       <div>
-        {count}
+        Count: {count}
+        Count2: {count2}
       </div>
-      <button type='button' onClick={() => setCount((currentCount) => currentCount + 1)} >Increment</button>
+      <button type='button' onClick={() => {
+        setCount((currentState) => ({
+          count: currentState.count + 1,
+          count2: currentState.count2
+        }))
+      }}>Increment</button>
       <button type='button' onClick={() => setCount(count - 1)} >Decrement</button>
     </div>
   )
